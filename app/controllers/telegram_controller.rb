b1e676bcc988f8de5964f564f56d22e53e0f7130
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-01-22 22:08:13 -0300

    Added /frase feature

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -7,7 +7,30 @@ class TelegramController < Telegram::Bot::UpdatesController
   end
 
   def brizufrase!
+    tag = Tag.where(title: 'Bunny').first
+    if tag.nil?
+      respond_not_found
+      return
+    end
+
+    random_post = tag.posts.all.sample(1).first
+
+    if random_post.nil?
+      respond_not_found
+      return
+    end
+
+    respond_with :message, text: random_post.content
+  end
+
+  def frase!
     random_post = Post.all.sample(1).first
     respond_with :message, text: random_post.content
+  end
+
+  private
+
+  def respond_not_found
+    respond_with :message, text: 'No encontré ninguna frase'
   end
 end
