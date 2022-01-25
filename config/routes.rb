25e47825318f61294b40438739086622980be5f6
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-01-25 00:07:21 -0300

    Random routes for Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 require 'telegram/bot'
 
 Rails.application.routes.draw do
+  controller :posts do
+    get '/posts/random' => :random
+    get '/posts/random/:tag' => :random_tagged
+  end
+
   resources :tags
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
